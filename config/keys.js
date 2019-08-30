39469f3ee704301c9b42280c6494a2684522a2b2
Altaha Ansari <altaha.altaha@gmail.com>
2019-08-30 18:13:47 +0530

    fixed DB key

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://micronoobDB:<password>@blogcluster-8ccvr.mongodb.net/test?retryWrites=true&w=majority" ||
+    mongoURI: "mongodb+srv://micronoobDB:micronoobdbpass@blogcluster-8ccvr.mongodb.net/test?retryWrites=true&w=majority" ||
         "mongodb://127.0.0.1:27017/blog"
 };
